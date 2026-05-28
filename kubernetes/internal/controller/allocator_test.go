a9ff4c90cd8763207198676b0968b0a967bd7214
longsuizhi <95574295+longsuizhi@users.noreply.github.com>
2026-05-28 16:02:30 +0800

    test(allocator): add unit test for dead pod re-allocation

--- kubernetes/internal/controller/allocator_test.go
+++ kubernetes/internal/controller/allocator_test.go
@@ -237,6 +237,29 @@ func TestSchedule(t *testing.T) {
 			},
 		},
 		{
+			name: "dead pod triggers re-allocation - allocated pod deleted externally",
+			spec: &AllocSpec{
+				Pods: []*corev1.Pod{
+					// pod1 was allocated but is now gone (not in Pods list).
+					// pod2 is a new available pod in the pool.
+					{ObjectMeta: metav1.ObjectMeta{Name: "pod2"}, Status: corev1.PodStatus{Phase: corev1.PodRunning, Conditions: []corev1.PodCondition{{Type: corev1.PodReady, Status: corev1.ConditionTrue}}}},
+				},
+				Pool: &sandboxv1alpha1.Pool{ObjectMeta: metav1.ObjectMeta{Name: "pool1"}},
+				Sandboxes: []*sandboxv1alpha1.BatchSandbox{
+					{ObjectMeta: metav1.ObjectMeta{Name: "sbx1"}, Spec: sandboxv1alpha1.BatchSandboxSpec{Replicas: &replica1}},
+				},
+			},
+			poolAlloc:     &PoolAllocation{PodAllocation: map[string]string{"pod1": "sbx1"}},
+			sandboxAllocs: map[string]*SandboxAllocation{"sbx1": {Pods: []string{"pod1"}}},
+			releases:      map[string]*AllocationRelease{"sbx1": {Pods: []string{}}},
+			released:      map[string]*AllocationReleased{"sbx1": {Pods: []string{}}},
+			wantAction: &algorithm.AllocAction{
+				ToAllocate:    map[string][]string{"sbx1": {"pod2"}},
+				ToRelease:     map[string][]string{"sbx1": {"pod1"}},
+				PodSupplement: 0,
+			},
+		},
+		{
 			name: "orphan sandbox - pods in store but sandbox no longer in spec",
 			spec: &AllocSpec{
 				Pods: []*corev1.Pod{
